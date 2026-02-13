d64b471f246b25e427db8af3615ff808743d5a17
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-02-14 02:00:12 +0400

    Add script to count and display command line arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    count = len(argv)
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    for i in range(count):
+        print("{}: {}".format(i + 1, argv[i]))
