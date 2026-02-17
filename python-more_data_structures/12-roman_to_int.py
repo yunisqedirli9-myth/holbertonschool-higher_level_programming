6dea28935caa0d95bf44dab1ac6f66ed0c920c7f
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-02-17 18:44:41 +0400

    Improve formatting of values dictionary in roman_to_int
    Refactor values dictionary for better readability.

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,16 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
 
-    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    values = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
     total = 0
 
     for i in range(len(roman_string)):
         v = values.get(roman_string[i], 0)
-        if i + 1 < len(roman_string) and v < values.get(roman_string[i + 1], 0):
+        next_v = values.get(roman_string[i + 1], 0) if i + 1 < len(roman_string) else 0
+        if v < next_v:
             total -= v
         else:
             total += v
