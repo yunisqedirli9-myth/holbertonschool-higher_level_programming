89065d600fd6a36f0d265e1fe396d273fa300027
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-03-08 03:46:57 +0400

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list, and then
+"""
+Module that adds all arguments to a Python list, and then
 save them to a file
 """
 import sys
 import os.path
-
 
 save_file = __import__('7-save_to_json_file').save_to_json_file
 load_file = __import__('8-load_from_json_file').load_from_json_file
