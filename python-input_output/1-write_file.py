a7b64930adfb501ddc339e70bdb2f363ab20a3fa
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-03-07 18:43:30 +0400

    Replace number_of_lines with write_file function
    Removed the number_of_lines function and added a write_file function to write a string to a file.

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the number of lines
-    of a text file
-"""
+"""Define a function to write a string to a file"""
 
 
-def number_of_lines(filename=""):
-    """ Function that reads from a file and prints its number of lines
-
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
-    """
-    n_lines = 0
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            n_lines += 1
-    return n_lines
+def write_file(filename="", text=""):
+    """function to write string"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
