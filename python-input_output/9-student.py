49dc0d4ff3fb4c05dcd7b7cd3227a247c578dad8
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-03-08 03:50:56 +0400

    Add Student class with initialization and JSON method

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Student class."""
+
+
+class Student:
+    """Define a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieve a dictionary representation of a Student instance."""
+        return self.__dict__
