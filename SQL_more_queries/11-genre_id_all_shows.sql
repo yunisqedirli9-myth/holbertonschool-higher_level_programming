22f180b29bafa6135b2a93be7709fda295b6e9eb
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-03-11 23:39:21 +0400

    Add query to list all shows with genre IDs
    Lists all shows with their titles and genre IDs, including those without a genre.

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows from hbtn_0d_tvshows, displaying title and genre_id.
+-- Shows without a genre display NULL. Results sorted by title and genre_id.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
