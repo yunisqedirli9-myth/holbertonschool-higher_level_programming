822b9d8f630f1180074435cb2aed3705dd46d4a0
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-03-15 18:26:25 +0400

    Refactor state selection to fetch all states
    Refactor database connection and query to select all states ordered by id.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-import sys
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
-
+import sys
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c= db.cursor()
-    c.execute("SELECT 8 FROM `states`")
-    [print(state) for state in c.fetchall()]
-    
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_user,
+        passwd=mysql_password,
+        db=db_name
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
