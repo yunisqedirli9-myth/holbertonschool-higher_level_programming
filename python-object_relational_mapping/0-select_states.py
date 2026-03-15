1a7655870450b98b2cc53879db5a996ff4dcbb14
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-03-15 18:21:16 +0400

    Add script to select states from MySQL database
    Connect to a MySQL database and select a specific state.

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,9 @@
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c= db.cursor()
+    c.execute("SELECT 8 FROM `states`")
+    [print(state) for state in c.fetchall()]
+    
