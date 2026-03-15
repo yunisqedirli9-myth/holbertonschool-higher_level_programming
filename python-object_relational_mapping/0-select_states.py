31c6b829db6473420187537fd0630301be3f795b
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-03-15 18:23:39 +0400

    Refactor state selection script for better clarity
    Refactor database connection and query execution to use parameters and improved structure.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,36 @@
-import sys
+#!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c= db.cursor()
-    c.execute("SELECT 8 FROM `states`")
-    [print(state) for state in c.fetchall()]
-    
+    # Komanda sətri arqumentlərini alırıq
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # MySQL serverinə qoşuluruq
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Sorğuları icra etmək üçün cursor yaradırıq
+    cursor = db.cursor()
+
+    # Məlumatları çəkirik
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Bütün nəticələri alırıq
+    rows = cursor.fetchall()
+
+    # Nəticələri çap edirik
+    for row in rows:
+        print(row)
+
+    # Bağlantıları bağlayırıq
+    cursor.close()
+    db.close()
