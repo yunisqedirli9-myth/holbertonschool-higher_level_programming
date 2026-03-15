6357f91224565ee464d00435ac301845a9588620
yunisqedirli9-myth <yunisqedirli9@email.com>
2026-03-15 19:45:35 +0000

    Add Task 14: Fetch cities and states using SQLAlchemy JOIN

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""Prints all City objects from the database hbtn_0e_14_usa"""
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+"""
 import sys
-from model_state import Base, State
-from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Verilənlər bazasına qoşuluruq
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # State və City cədvəllərini JOIN edirik
-    results = session.query(State, City).filter(
-        State.id == City.state_id).order_by(City.id).all()
-
+    
+    # State və City cədvəllərini birləşdirib (JOIN), City ID-sinə görə sıralayırıq
+    results = session.query(State, City).filter(State.id == City.state_id).order_by(City.id).all()
+    
+    # Nəticələri "State Name: (City ID) City Name" formatında çap edirik
     for state, city in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-
+        
     session.close()
