04ee44f83d1eb690b44d135a5862e2b429e72e2d
yunisqedirli9-myth <yunisqedirli9@email.com>
2026-03-15 19:35:17 +0000

    filt

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-"""Lists all cities of a state from hbtn_0e_4_usa database"""
+"""
+Script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa.
+"""
 import sys
 import MySQLdb
 
-
-
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    conn = MySQLdb.connect(
+    # Arqumentlərdən istifadə edərək bazaya qoşuluruq
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    cur = conn.cursor()
-    query = "SELECT cities.name FROM cities "
-    query += "JOIN states ON cities.state_id = states.id "
-    query += "WHERE states.name = %s "
-    query += "ORDER BY cities.id ASC"
-    cur.execute(query, (state_name,))
-    query_rows = cur.fetchall()
-
-    city_names = [row[0] for row in query_rows]
-    print(", ".join(city_names))
-
-    cur.close()
-    conn.close()
-
+    cursor = db.cursor()
+    
+    # Ştat adına görə şəhərləri tapmaq üçün JOIN sorğusu
+    query = "SELECT cities.name FROM cities " \
+            "JOIN states ON cities.state_id = states.id " \
+            "WHERE states.name = %s " \
+            "ORDER BY cities.id ASC"
+    
+    cursor.execute(query, (sys.argv[4],))
+    
+    # Bütün nəticələri alırıq
+    rows = cursor.fetchall()
+    
+    # Şəhər adlarını vergüllə ayıraraq çap edirik
+    print(", ".join(row[0] for row in rows))
+    
+    cursor.close()
+    db.close()
